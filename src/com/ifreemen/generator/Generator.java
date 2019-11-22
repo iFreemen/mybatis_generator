9c2e6188454f0626da9cf2bccd030c9a024a782e
Freemen <903944126@qq.com>
2019-11-22 15:59:32 +0800

    逆向工程

---
+++ src/com/ifreemen/generator/Generator.java
@@ -0,0 +1,39 @@
+package com.ifreemen.generator;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author Freemen
+ * @Date 2019-04-28 09:00
+ * @email 903944126@qq.com
+ */
+
+/**
+ * The following code sample shows how to call MBG from Java with an XML based configuration.
+ * It does not show exception handling, but that should be obvious from the compiler errors :)
+ */
+public class Generator {
+
+    public static void main(String[] args) throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
+        List<String> warnings = new ArrayList<>();
+        boolean overwrite = true;
+        File configFile = new File("generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
+
